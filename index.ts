4e330b1c6cb3a0f334a43b19e4d724815a0f4150
mahbodsr <srmahbod@gmail.com>
2024-06-15 14:44:43 +0330

    TON Added | Currency from rial to toman

--- index.ts
+++ index.ts
@@ -14,11 +14,17 @@ bot.command("current", async (ctx) => {
   if (ctx.chat.id !== +process.env.CHAT_ID!) return;
   try {
     const res = await axios.get(process.env.GET_PRICE_LINK!);
-    ctx.reply(
-      `NOT/RIAL: \t${+res.data.stats["not-rls"].latest}\nNOT/USDT: \t${+res.data
-        .stats["not-usdt"].latest}`,
-      { reply_parameters: { message_id: ctx.msg.message_id } }
-    );
+    let prices = "";
+    prices +=
+      "NOT/TOMAN: \t" +
+      (+res.data.stats["not-rls"].latest / 10).toLocaleString();
+    prices += "\nNOT/USDT: \t" + res.data.stats["not-usdt"].latest;
+    prices +=
+      "\nTON/TOMAN: \t" +
+      (+res.data.stats["ton-rls"].latest / 10).toLocaleString();
+    prices += "\nTON/USDT: \t" + res.data.stats["ton-usdt"].latest;
+
+    ctx.reply(prices, { reply_parameters: { message_id: ctx.msg.message_id } });
   } catch {
     ctx.reply("Internal Error.\nCall @MahBodSr");
   }
